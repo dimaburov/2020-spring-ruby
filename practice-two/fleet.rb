ec5c456bfffcc63ac1e3b5af89c8188d0e572d0b
burov dmitry <dimaburov15@gmail.com>
2020-03-22 17:33:09 +0300

    start two program

--- practice-two/fleet.rb
+++ practice-two/fleet.rb
@@ -2,13 +2,51 @@ class Fleet
     def initialize()
         @cars=[]
     end
-    def add(Car)
-        @cars.push=Car
+    def add(car)
+        @cars.push=car
+        puts @cars
     end
     def load_from_file(file_name)
         require 'json'
      json_data = File.read(file_name)
      ruby_objects = JSON.parse(json_data)
-     pp  ruby_objects
+     while(ruby_objects['mark']!=nil) do
+        add(Auto.new(ruby_objects['mark'],ruby_objects['model'],ruby_objects['year'],ruby_objects['consumption']))
+     end
+    end
+    def average_consumption ()
+        @cars.each do |kol|
+            allsum=allsum+@cars[kol].gasoline_consumption
+        end
+       return average=allsum/@cars.size
+    end
+
+    def number_by_brand (brand)
+        count=0
+        @cars.each do |kol|
+            if(@cars[kol].brand==brand) then count=count+1
+             end
+        end
+        return count
+    end
+
+    def number_by_model(model)
+        count=0
+        @cars.each do |kol|
+            if(@cars[kol].model==model) then count=count+1
+             end
+        end
+        return count
+    end
+    def consumption_by_brand(brend)
+        count=0
+        allsum.to_f
+        @cars.each do |kol|
+            if(@cars[kol].brand==brand) then 
+                allsum=@cars[kol].gasoline_consumption+allsum
+                count=count+1
+            end
+         return average=allsum/count
+        end
     end
 end
--- practice-two/main.rb
+++ practice-two/main.rb
@@ -1,5 +1,9 @@
 require_relative 'auto.rb'
+require_relative 'fleet.rb'
 object_one=Auto.new("BMW","X5 3,0D",2017,9.5845574007079)
 object_two=Auto.new("Cherry","A5 2,0i",2018,8.759773242361172)
 puts object_one.to_s
 puts object_two.to_s
+object_ine=add(object_one)
+object_two.add
+# load_from_file(C:\Temp\myproject\2020-spring-ruby\practice-two\NewDocument1.json)
