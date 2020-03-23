4ea5f2563c0aa7462dfb8af1a64f02264b48085a
burov dmitry <dimaburov15@gmail.com>
2020-03-23 13:43:07 +0300

    Practic one - Practic two Finish

--- practice-two/fleet.rb
+++ practice-two/fleet.rb
@@ -3,50 +3,60 @@ class Fleet
         @cars=[]
     end
     def add(car)
-        @cars.push=car
-        puts @cars
+        @cars.push(car)
     end
     def load_from_file(file_name)
         require 'json'
-     json_data = File.read(file_name)
+      json_data = File.read(file_name)
      ruby_objects = JSON.parse(json_data)
-     while(ruby_objects['mark']!=nil) do
-        add(Auto.new(ruby_objects['mark'],ruby_objects['model'],ruby_objects['year'],ruby_objects['consumption']))
+     kol=0
+     while(ruby_objects.size!=kol) do
+        add(Auto.new(ruby_objects[kol]['mark'],ruby_objects[kol]['model'],ruby_objects[kol]['year'],ruby_objects[kol]['consumption']))
+        kol=kol+1
      end
     end
     def average_consumption ()
-        @cars.each do |kol|
-            allsum=allsum+@cars[kol].gasoline_consumption
+        kol=0
+        sumall=0.0
+        while(@cars.size!=kol) do
+            sumall=@cars[kol].gasoline_consumption.to_f+sumall
+            kol=kol+1
         end
-       return average=allsum/@cars.size
+       return average=sumall/@cars.size
     end
 
     def number_by_brand (brand)
         count=0
-        @cars.each do |kol|
+        kol=0
+        while(@cars.size!=kol) do
             if(@cars[kol].brand==brand) then count=count+1
              end
+             kol=kol+1
         end
         return count
     end
 
     def number_by_model(model)
         count=0
-        @cars.each do |kol|
+        kol=0
+        while(@cars.size!=kol) do
             if(@cars[kol].model==model) then count=count+1
              end
+             kol=kol+1
         end
         return count
     end
-    def consumption_by_brand(brend)
+    def consumption_by_brand(brand)
         count=0
-        allsum.to_f
-        @cars.each do |kol|
+        allsum=0.0
+        kol=0
+        while(@cars.size!=kol) do
             if(@cars[kol].brand==brand) then 
-                allsum=@cars[kol].gasoline_consumption+allsum
+                allsum=@cars[kol].gasoline_consumption.to_f+allsum
                 count=count+1
             end
-         return average=allsum/count
+            kol=kol+1
         end
+        return average=allsum/count
     end
 end
--- practice-two/main.rb
+++ practice-two/main.rb
@@ -2,8 +2,13 @@ require_relative 'auto.rb'
 require_relative 'fleet.rb'
 object_one=Auto.new("BMW","X5 3,0D",2017,9.5845574007079)
 object_two=Auto.new("Cherry","A5 2,0i",2018,8.759773242361172)
-puts object_one.to_s
-puts object_two.to_s
-object_ine=add(object_one)
-object_two.add
-# load_from_file(C:\Temp\myproject\2020-spring-ruby\practice-two\NewDocument1.json)
+puts object_one
+puts object_two
+object=Fleet.new()
+object.add(object_one)
+object.add(object_two)
+object.load_from_file('NewDocument1.json')
+puts object.average_consumption
+puts object.number_by_brand("BMW")
+puts object.number_by_model("X5 3,0D")
+puts object.consumption_by_brand("Cherry")
