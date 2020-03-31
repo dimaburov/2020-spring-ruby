022ff0b8e84e245ce43058070ac8f63eb0b0417e
burov dmitry <dimaburov15@gmail.com>
2020-03-31 15:12:37 +0300

    practic four my_complex

---
+++ practic-four/my_complex.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# works with complex numbers
+class MyComplex
+  def initialize(real, imaginary)
+    @real = real
+    @imaginary = imaginary
+  end
+
+  def to_s
+    "#{@real} + i*#{@imaginary}"
+  end
+
+  attr_reader :real
+
+  attr_reader :imaginary
+
+  def add(other)
+    MyComplex.new(@real.to_f + other.real.to_f, @imaginary.to_f + other.imaginary.to_f)
+  end
+
+  def subtraction(other)
+    MyComplex.new(@real.to_f - other.real.to_f, @imaginary - other.imaginary.to_f)
+  end
+  def division(other)
+    MyComplex.new(@real.to_f/other.real.to_f, @imaginary.to_f/other.imaginary.to_f)
+  end
+  def multiplication(other)
+    MyComplex.new(@real.to_f*other.real.to_f, @imaginary.to_f*other.imaginary.to_f)
+  end
+end
