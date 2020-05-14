1ec127f1c44c4b7ccaa553c4c6dee1e807d6d98a
Буров Дмитрий <dimaburov15@gmail.com>
2020-05-14 16:32:58 +0300

    site output data in sorted

--- Lab3/app.rb
+++ Lab3/app.rb
@@ -13,10 +13,12 @@ class App < Roda
     plugin :public
     opts[:serve_static] = true
   end
-  opts[:index] = DiaryList.new([
-                                 Diary.new('2020-04-05', 'Кен Кизи', 'Над кукушкиным гнездом'),
+  opts[:index] = DiaryList.new([ 
+                                 Diary.new('2018-03-07', 'AAA', 'BBB'),
                                  Diary.new('2019-05-10', 'Бёрджесс', 'Заводной апельсин'),
-                                 Diary.new('2018-03-06', 'Бредбери', 'Вино из одуванчиков')
+                                 Diary.new('2020-04-05', 'Кен Кизи', 'Над кукушкиным гнездом'),
+                                 Diary.new('2018-03-06', 'Бредбери', 'Вино из одуванчиков'),
+                                 Diary.new('2018-04-06', 'bbbb', 'aaa')
                                ])
   route do |r|
     r.public if opts[:serve_static]
@@ -27,6 +29,7 @@ class App < Roda
       r.is do
         # @params = InputValidators.check_min_and_max(r.params['min'], r.params['max'])
         # pp @params[:error]
+        opts[:index].sort_by_data
         @all_books = opts[:index].all_books
         view('index')
       end
--- Lab3/lib/diary_list.rb
+++ Lab3/lib/diary_list.rb
@@ -20,5 +20,29 @@ class DiaryList
     @books.dup
   end
 
-  def sort_by_data; end
+  def sort_by_data
+    # arr=[]
+    # @books.map { |book|  book.data.each_line('-') { |s|  array.append(s.delete "-")}}
+    # array=@books.sort {|a,b|  a.data<=>b.data}
+    # arr= @books.sort { |a, b|  b.data.each_line('-'){ |s|  s.delete "-"}.to_i<=>a.data.each_line('-') { |s| s.delete "-"}.to_i }
+    # array=[]
+    # array= arr.sort {|a,b| b.data.each_line('-'){ |s|  s.delete "-"}.to_i<=>a.data.each_line('-') { |s| s.delete "-"}.to_i }
+    # @books.map do |book|
+    #     p array.append(book.data.slice(8..9))
+    # end
+    @books= @books.sort do |a, b| 
+        if (b.data.slice(0..3).to_i<=>a.data.slice(0..3).to_i)==0 then
+            if (b.data.slice(5..6).to_i<=>a.data.slice(5..6).to_i)==0 then
+                b.data.slice(8..9).to_i<=>a.data.slice(8..9).to_i
+            else b.data.slice(5..6).to_i<=>a.data.slice(5..6).to_i
+            end
+        else b.data.slice(0..3).to_i<=>a.data.slice(0..3).to_i
+        end 
+    end
+    p @books
+    # arr= arr.sort {|a,b| b.data.slice(5..6).to_i<=>a.data.slice(5..6).to_i}
+    # p arr
+    # arr= arr.sort {|a,b| b.data.slice(8..9).to_i<=>a.data.slice(8..9).to_i}
+    # p arr 
+  end
 end
